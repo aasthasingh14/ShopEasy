d32291488460e3015c2f93cc3ac19da42a3632f7
aasthasingh14 <aasthasingh14112002@gmail.com>
2024-04-09 09:05:53 +0530

    first commit

--- src/Components/ShopList.js
+++ src/Components/ShopList.js
@@ -1,33 +1,41 @@
-import React, { useState, useEffect } from "react";
+//step 1 : import libaries
+
+import { Component } from "react";
+import React from "react";
 import { ScrollView, Text, View } from "react-native";
-import axios from "axios";
+import axios from "axios"; 
 import ShopDetails from "./ShopDetails";
+//step 2 : create component 
 
-const ShopList = () => {
-  const [shopList, setShopList] = useState([]);
+//class component
+class ShopList extends Component{
 
-  useEffect(() => {
-    axios
-      .get("https://my-json-server.typicode.com/aasthasingh14/shopNow/products")
-      .then((response) => {
-        setShopList(response.data);
-      })
-      .catch((error) => {
-        console.error("Error fetching data: ", error);
-      });
-  }, []);
+    state = {shopList:[]}
 
-  const renderList = () => {
-    return shopList.map((product, index) => {
-      return <ShopDetails key={index} product={product} />;
-    });
-  };
+    componentDidMount() {
+        // console.log("Called from cdm")
+        axios.get('https://my-json-server.typicode.com/aasthasingh14/shopNow/products')
+        .then((response)=>{
+            this.setState({shopList:response.data})
+        })
+    }
+    
+    renderList = () => {
+        return this.state.shopList.map((product, index) => {
+            return <ShopDetails key={index} product={product} />;
+        });
+    }
+    
 
-  return (
-    <ScrollView>
-      {renderList()}
-    </ScrollView>
-  );
-};
+    render(){
+        console.log(this.state)
+        return (
+            // <View> {this.renderList()} </View>
+            <ScrollView>{this.renderList()}</ScrollView>
+        ) 
+    }
+}
 
-export default ShopList;
+//step 3:  make component available to other component
+
+export default ShopList
